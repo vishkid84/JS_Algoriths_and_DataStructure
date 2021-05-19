fa34aa484d1e02ed8b11672b1f52e1c5fff1d834
vishkid84 <vishalittr@gmail.com>
2021-05-19 19:13:09 +0000

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,33 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//     let stringed = n.toString().split('');
+//     if(stringed[0] === '-'){
+//         let removed = stringed.shift(); //this removes the first item in the array, so removed in this case is '-'
+//         let reversed = removed + stringed.reverse().join('');
+//         n = parseInt(reversed);
+//         return n;
+//     }
+//     n = parseInt(stringed.reverse().join(''))
+//     return n;
+// }
+
+// function reverseInt(n) {
+//    let reversed = n.toString().split('').reverse().join('');
+//    if(n<0){
+//       return parseInt(reversed) * -1;
+//    }
+//    return parseInt(reversed);
+// }
+
+function reverseInt(n) {
+   let reversed = n.toString().split('').reverse().join('');
+   return parseInt(reversed) * Math.sign(n);
+}
+
+reverseInt(35);
+
+reverseInt(-30);
 
 module.exports = reverseInt;
