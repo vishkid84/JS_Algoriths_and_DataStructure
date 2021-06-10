bb1207951076d40f5dbba28935c4a69c8f8826c0
vishkid84 <vishalittr@gmail.com>
2021-06-10 10:28:23 +0000

    anagrams updated

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -43,30 +43,42 @@
 
 
 // ------------Using chain function----------
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+
+//     if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+//         return false;
+//     }
+
+//     for(let char in aCharMap){
+//         if(aCharMap[char] !== bCharMap[char]){
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+// function buildCharMap(str){
+//     const charMap = {}
+//     for(let char of str.replace(/[^\w]/g, "").toLowerCase()){
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     return charMap;
+// }
+
+
+// -------Using sort-----------
 function anagrams(stringA, stringB) {
-    const aCharMap = buildCharMap(stringA);
-    const bCharMap = buildCharMap(stringB);
-
-    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
-        return false;
-    }
-
-    for(let char in aCharMap){
-        if(aCharMap[char] !== bCharMap[char]){
-            return false;
-        }
-    }
-
-    return true;
+    return cleanString(stringA) === cleanString(stringB);
 }
 
-function buildCharMap(str){
-    const charMap = {}
-    for(let char of str.replace(/[^\w]/g, "").toLowerCase()){
-        charMap[char] = charMap[char] + 1 || 1;
-    }
+function cleanString(str){
+    string = str.replace(/[^\w]/g, "").toLowerCase().split('').sort().join();
 
-    return charMap;
+    return string;
 }
 
 module.exports = anagrams;
